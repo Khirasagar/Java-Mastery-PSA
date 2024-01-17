590dd0e2099d0b4c74d4308a00acffcf78d6bb25
khirasagar.maharana8100@gmail.com <khirasagar.maharana8100@gmail.com>
2024-01-17 09:31:45 +0530

    first commit

---
+++ app_java_1/A.java
@@ -0,0 +1,41 @@
+package app_java_1;
+
+//public class A {
+//	public static void main (String args[]) {
+//		A a1 = new A();
+//		a1.test();
+//		a1.test();
+//		a1.test();
+//	}
+//	public void test() {
+//		System.out.println(100);
+//	} 
+//}
+
+
+//public class A{
+//	public static void main(String[] args) {
+//		A.test(100,"mike",10.3);
+//	}
+//	public static void test(int x, String y, double z) {
+//		System.out.println(x);
+//		System.out.println(y);
+//		System.out.println(z);
+//		
+//	}
+//}
+
+
+public class A{
+	public static void main(String[] args) {
+		A a1 = new A();
+		a1.test(100, "mike", 10.4);
+		
+	}
+	public void test(int x, String y, double z) {
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+	}
+}
